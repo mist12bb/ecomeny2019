b46eb1b0af8e9c8a7034b81d2357d3a05998df98
nana <room102home@gmail.com>
2018-12-09 10:42:08 +0200

    action box setup | start.
    Signed-off-by: nana <room102home@gmail.com>

---
+++ app/components/actions/action-box.js
@@ -0,0 +1,61 @@
+import React from "react";
+import {  Feed, Icon } from 'semantic-ui-react'
+import { withState } from "recompose";
+import RemoveModal from "./modals/remove.modal";
+
+
+import "./act.style.css"
+import ActionInit from "./action.init";
+import ActionPutInit from "./actions.put.init";
+
+
+
+ const Action = ({onClickDel, onClickSave, i, action, onInit, init, onInilze, category, setActions}) =>  {
+  return (
+    <Feed.Event key={i} >
+             <Feed.Label
+              className={"amount-lable"}
+              >
+              <p
+             className={"amount-lable__text"}
+              children={action? action.amount:33}
+             />
+             </Feed.Label>
+             <Feed.Content>
+                <Feed.Date content={new Date (action.date).toLocaleDateString()}/>
+                <Feed.Summary>
+                <p>{action? action.info:'descripton gose here...'}</p>
+                </Feed.Summary>
+                <Feed.Event >
+                {/* < Checkbox label="add to initlze"   checked={  init}   onChange={(e)=>{
+                  console.log(init, action.init, "action init")
+                  onInit(!init);
+                  onInilze(action.id, init, action);
+
+                }}></ Checkbox> */}
+               <ActionInit init={action.init}/>
+                </Feed.Event>
+             </Feed.Content>
+             <Feed.Meta>
+                <ActionPutInit action={action} category={category} setActionsOnInitAction={(data)=>{
+                  setActions(data)
+                }}/>
+             <RemoveModal className={"action-box__rem-btn"} onDeleteAction={()=>{onClickDel(i)}}>
+             {/* <Icon name="remove circle"/> */}
+             </RemoveModal>
+             <button style={{all: "unset", cursor:"pointer"}}
+             onClick={(e)=>{
+               onClickSave(action)
+             }}
+             >
+             <Icon name="save" />
+             </button>
+             </Feed.Meta>
+            </Feed.Event>
+
+  )
+}
+
+
+
+export default withState("init", "onInit", false)(Action);
