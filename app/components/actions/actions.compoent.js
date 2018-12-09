de3a5702534b84c7792b9ddc5f8420437feea5f8
nana <room102home@gmail.com>
2018-12-09 21:36:52 +0200

    actions all || setup | start.
    Signed-off-by: nana <room102home@gmail.com>

---
+++ app/components/actions/actions.compoent.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Feed, Card } from 'semantic-ui-react';
+
+const Actions = ({actions, propsAll}) => {
+
+  let {} = propsAll;
+
+  return (
+    <div className="actions-compoentent">
+      <div className="actions-compoentent__card">
+          <Card>
+            <Card.Content>
+
+              <Card.Hader>
+              </Card.Hader>
+
+            </Card.Content>
+                
+            <Card.Content>
+            </Card.Content>
+          </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Actions;
